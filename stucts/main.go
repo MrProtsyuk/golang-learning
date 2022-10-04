ffdef765a5dcbbfac477243d5f920fd1293acaf9
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-10-03 20:39:42 -0700

    created an embedded struct

--- stucts/main.go
+++ stucts/main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contactInfo
 }
+
 
 //use this type if you want to access "0 value" of a variable
 // func main() {
@@ -20,7 +27,19 @@ type person struct {
 
 // syntax here is not standard but it gives more leaniancy with variables
 func main() {
-	mark := person{firstName: "Mark", lastName: "Protsyuk"}
+	mark := person{
+		firstName: "Mark", 
+		lastName: "Protsyuk",
+		// embedded stuct
+		contactInfo: contactInfo{
+			email: "Mark@gmail.com",
+			zipCode: 12345,
+		},
+	}
 
+	// This shows value and variable
+	// fmt.Printf("%+v", mark)
+
+	// this just shows value
 	fmt.Println(mark)
 }
