5e4a49f062b20e1cb857bb564d89e5098320ff12
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-10-03 19:41:16 -0700

    created a deck in Go with shuffle func and testing

--- deck-of-cards/deck.go
+++ deck-of-cards/deck.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
+// create a new type of deck which is slice of strings
 type deck []string
 
+//combinging two slices into one
 func newDeck() deck {
 	cards := deck{}
 
@@ -19,12 +28,48 @@ func newDeck() deck {
 	return cards
 }
 
+//printing
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 } 
 
+//cutting a slice
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+//joining a slice
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+//saving to file
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+//spliting a byte slice
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+//random number generation
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
